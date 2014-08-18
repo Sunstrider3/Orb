fab3cbcecc88ffdb4ff2f264ba8410aff1156d74
Jesse Taiapa <beardythepirate@gmail.com>
2014-08-18 23:03:43 +1200

    More third person shit done

---
+++ Orb/Assets/3rd Person Test/CameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour 
+{
+    public float lookSpeed;
+    public Transform player;
+
+    private Transform myTransform;
+
+    void Start ()
+    {
+        myTransform = this.transform;
+	}
+	
+	void Update ()
+    {
+        float xDistance = Input.GetAxis("Mouse X") * lookSpeed;
+        float yDistance = Input.GetAxis("Mouse Y") * lookSpeed;
+        Quaternion target = Quaternion.Euler(
+            myTransform.eulerAngles.x - yDistance,
+            myTransform.eulerAngles.y + xDistance,
+            myTransform.eulerAngles.z);
+
+        myTransform.rotation = target;
+
+        myTransform.position = new Vector3(
+            player.position.x,
+            player.position.y + 1.75f,  //moves camera up to look at head
+            player.position.z);
+
+    }
+}
---
+++ Orb/Assets/3rd Person Test/ThirdPersonController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonController : MonoBehaviour
+{
+    public Animator anim;
+    public Transform myCamera;
+    public float lookSpeed = 1f;
+    public float rotationSpeed = 360f;
+    public float maxSpeed = 2;
+
+    // private CharacterController controller;
+    private Transform myTransform;
+
+	void Start ()
+    {
+        //controller = GetComponent<CharacterController>();
+        myTransform = this.transform;
+	}
+	
+	void Update ()
+    {
+        Move();
+        //UpdateCamera();
+
+	}
+
+    private Quaternion targetRotation;
+    private float speed;
+    private void Move()
+    {
+
+        Vector3 input = new Vector3(
+            Input.GetAxis("Horizontal"),
+            0,
+            Input.GetAxis("Vertical"));
+
+        Vector3 motion = myCamera.TransformDirection(input);
+        motion = Vector3.ClampMagnitude(motion, 1f);
+
+        if (input != Vector3.zero)
+        {
+            targetRotation = Quaternion.LookRotation(motion);
+            transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetRotation.eulerAngles.y, rotationSpeed * Time.deltaTime);
+        }
+
+        //anim.SetBool("Jump", Input.GetButtonDown("Jump"));
+        
+        speed += (Input.GetButton("Sprint")) ? Time.deltaTime : -Time.deltaTime;
+        speed = (speed <= 1) ? motion.magnitude : Mathf.Clamp(speed, 1, maxSpeed);
+
+        // anim.SetFloat("Speed", Mathf.Abs(motion.magnitude));
+        anim.SetFloat("Speed", speed);
+    }
+
+    private void UpdateCamera()
+    {
+
+        Quaternion target = Quaternion.Euler(
+            myCamera.rotation.x,
+            myCamera.rotation.y + Input.GetAxis("Mouse X"),
+            myCamera.rotation.z);
+
+        myCamera.rotation = target;
+        myCamera.position = myTransform.position;
+    }
+}
